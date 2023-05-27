3151c201296fbd9c631cfc03a5b9ad79f3225ec7
fatema-nasrin <nasrinfatema35@gmail.com>
2023-05-27 10:29:39 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// middleware
+app.use(cors())
+app.use(express.json());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4dbhsix.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const toyCollection = client.db('allToy').collection('toys');
+
+     // allToys routes
+     app.get('/allToys', async (req, res) => {
+      const cursor = toyCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+  })
+
+  app.get('/allToys/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id)
+     }
+
+    const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { price:1, rating:1,picture:1,quantity:1,description:1 },
+    };
+
+    const result = await toyCollection.findOne(query, options);
+    res.send(result);
+})
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/' ,(req,res) => {
+    res.send('toy marketplace server is running')
+})
+
+app.listen(port, () => {
+    console.log(`toy marketplace server is running on port: ${port}`);
+})
